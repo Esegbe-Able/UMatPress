59dc16d0414e82b6e498d3c8618197b79beec854
Esegbe-Able <112277036+Esegbe-Able@users.noreply.github.com>
2023-10-16 21:29:43 +0000

    Add files via upload

---
+++ dashboard/group-members.php
@@ -0,0 +1,15 @@
+<article class="item item3 interface" id="tab3">
+
+  <div class="post-available">
+    <div class="purple-design">
+      <h1>Contributors </h1>
+    </div>
+  </div>
+
+  <div class="content">
+    <div class="list">
+      <div class="list-item">Owusu Charles Bih</div>
+    </div>
+
+  </div>
+</article>
